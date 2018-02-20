ee769f0cae9026c3e6d2180f3b7178fbd23e613f
sunilvarmasv <33340008+sunilvarmasv@users.noreply.github.com>
2018-02-20 21:11:31 +0530

    Comitting to local repo

---
+++ testngbatch/src/testngbatch/NewTest.java
@@ -0,0 +1,26 @@
+package testngbatch;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f1() {
+	  System.out.println("This is from batch");
+	  
+  }
+  
+  @Test
+  public void f2() {
+	  System.out.println("This is from batch , this will be skipped");
+	  
+  }
+  
+  @Test
+  public void f3() {
+	  System.out.println("This is from batch , this will not  be skipped");
+	  
+  }
+  
+  
+  
+}
---
+++ testngbatch/src/testngbatch/NewTest2.java
@@ -0,0 +1,10 @@
+package testngbatch;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f4() {
+	  System.out.println("This is from new class");
+  }
+}
